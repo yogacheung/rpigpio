627d43a97a100e28745326bebcddbca68c6ce51f
yogacheung <yauka0215@gmail.com>
2019-10-13 23:26:02 -0500

    v1.0.0

--- js/login.js
+++ js/login.js
@@ -1,6 +1,5 @@
 function login(name, password, callback){
   $.ajax({    
-    //url: "http://808.bdcode.com:8580/php/login.php?username="+name+"&password="+password,    
     url: "php/login.php?username="+name+"&password="+password,    
   })
   .done(callback);
@@ -13,7 +12,6 @@ function onlogin() {
   var gpw = document.getElementById("password").value;
   
 	login(gname, gpw, function(res){
-		  
 		if(res == "no"){
 			alert("Name or/and Password are incorrect!");
 		}else{
