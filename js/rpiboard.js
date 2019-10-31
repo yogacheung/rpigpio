6d67edbd79abd9a77f5a5228ba745d18db886019
yogacheung <yauka0215@gmail.com>
2019-10-30 22:54:38 -0500

    fix cross origin

--- js/rpiboard.js
+++ js/rpiboard.js
@@ -16,7 +16,7 @@ function loadpinout() {
   loaddata(function(data){
     //console.log(data);
     var pinlist = data;
-    console.log(pinlist[0]);
+    //console.log(pinlist[0]);
     
     var numpin = {2:3, 3:5, 4:7, 5:29, 6:31, 7:26, 8:24, 9:21, 10:19, 11:23, 12:32, 13:33, 14:8, 15:10, 16:36, 17:11, 18:12, 19:35, 20:38, 21:40, 22:15, 23:16, 24:18, 25:22, 26:37, 27:13}
     var pinstate = ["Off","Off","Off","Off","Off","Off","Off","Off","Off","Off","Off","Off","Off","Off","Off","Off","Off","Off","Off","Off","Off","Off","Off","Off","Off","Off","Off","Off","Off","Off","Off","Off","Off","Off","Off","Off","Off","Off","Off","Off"];
